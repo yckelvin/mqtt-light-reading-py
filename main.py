3e530c785dbf00cb27f40733139b1f1dd63b6d56
Kelvin Lee <yckelvin@users.noreply.github.com>
2022-08-11 15:07:24 +0800

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
+def on_button_pressed_a():
+    microIoT.microIoT_SendMessage(convert_to_text(pins.analog_read_pin(AnalogPin.P0)),
+        microIoT.TOPIC.TOPIC_0)
+    microIoT.microIoT_showUserText(2,
+        "Light:" + convert_to_text(pins.analog_read_pin(AnalogPin.P0)))
+    basic.pause(100)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
 wifi_name = "izowifi"
 password = "izo1234@"
 iot_id = "lmZB9bXGR"
 iot_pwd = "liWfrxXMgz"
 topic_0 = "qwPmNL37g"
+basic.show_number(0)
 microIoT.microIoT_initDisplay()
 microIoT.microIoT_WIFI(wifi_name, password)
+basic.show_number(1)
 microIoT.microIoT_MQTT(iot_id, iot_pwd, topic_0, microIoT.SERVERS.ENGLISH)
+basic.show_number(2)
 microIoT.microIoT_showUserText(0, wifi_name)
 
 def on_forever():
-    microIoT.microIoT_SendMessage(convert_to_text(pins.analog_read_pin(AnalogPin.P0)),
-        microIoT.TOPIC.TOPIC_0)
-    microIoT.microIoT_showUserText(2,
-        "Light:" + convert_to_text(pins.analog_read_pin(AnalogPin.P0)))
+    pass
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,13 +1,20 @@
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    microIoT.microIoT_SendMessage(convertToText(pins.analogReadPin(AnalogPin.P0)), microIoT.TOPIC.topic_0)
+    microIoT.microIoT_showUserText(2, "Light:" + convertToText(pins.analogReadPin(AnalogPin.P0)))
+    basic.pause(100)
+})
 let wifi_name = "izowifi"
 let password = "izo1234@"
 let iot_id = "lmZB9bXGR"
 let iot_pwd = "liWfrxXMgz"
 let topic_0 = "qwPmNL37g"
+basic.showNumber(0)
 microIoT.microIoT_initDisplay()
 microIoT.microIoT_WIFI(wifi_name, password)
+basic.showNumber(1)
 microIoT.microIoT_MQTT(iot_id, iot_pwd, topic_0, microIoT.SERVERS.English)
+basic.showNumber(2)
 microIoT.microIoT_showUserText(0, wifi_name)
 basic.forever(function on_forever() {
-    microIoT.microIoT_SendMessage(convertToText(pins.analogReadPin(AnalogPin.P0)), microIoT.TOPIC.topic_0)
-    microIoT.microIoT_showUserText(2, "Light:" + convertToText(pins.analogReadPin(AnalogPin.P0)))
+    
 })
